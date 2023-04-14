4caaffd34d6635beb7b3731dca001201978390c6
ghostITwe <vladislav.ilyinr@gmail.com>
2023-04-14 09:36:13 +0300

    Refactoring api.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('lastname');
+            $table->string('lastName');
             $table->string('name');
             $table->string('patronymic');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->char('phone', 11);
             $table->enum('role', ['Сотрудник', 'Студент', 'Школьник', 'Внешний партнер']);
-            $table->string('study_group');
+            $table->string('study_group')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- routes/api.php
+++ routes/api.php
@@ -3,23 +3,28 @@
 use App\Http\Controllers\{Api\AuthController, Api\PostController, Api\ProjectController};
 use Illuminate\Support\Facades\Route;
 
-Route::controller(AuthController::class)->group(function () {
-    Route::post('/registration', 'register');
-    Route::post('/login', 'login');
-    Route::post('/logout', 'logout')->middleware('auth:sanctum');
+
+Route::post('/registration', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/',[ProjectController::class, 'index']);
+
+Route::group(['prefix' => '/posts'], function () {
+    Route::get('/', [PostController::class, 'index']);
+    Route::get('/{id}', [PostController::class, 'show']);
 });
 
-Route::controller(ProjectController::class)->prefix('/projects')->group(function () {
-    Route::get('/','index');
-    Route::post('/create', 'store')->middleware('auth:sanctum');
-    Route::delete('/{id}/delete', 'destroy')->middleware('auth:sanctum');
-});
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::controller(PostController::class)->prefix('/posts')->group(function () {
-    Route::get('/', 'index');
-    Route::get('/{id}', 'show');
-    Route::post('/', 'store');
+    Route::group(['prefix' => '/posts'], function () {
+        Route::post('/', [PostController::class, 'store']);
+        Route::post('/{id}/like', [PostController::class, 'storeLike']);
+        Route::delete('/{id}/like', [PostController::class, 'deleteLike']);
+    });
 
-    Route::post('/{id}/like', 'storeLike')->middleware('auth:sanctum');
-    Route::delete('/{id}/like', 'deleteLike')->middleware('auth:sanctum');
+    Route::group(['prefix' => '/projects'], function () {
+        Route::post('/create', [ProjectController::class, 'store']);
+        Route::delete('/{id}/delete', [ProjectController::class, 'destroy']);
+    });
 });
